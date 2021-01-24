87cb21a1d734d0557775a80e292f4342fb8fcd8d
dmitrijLo <demilorenz@googlemail.com>
2021-01-24 12:09:53 +0100

    testing canvasinteractor

--- docs/js/tractrix.js
+++ docs/js/tractrix.js
@@ -1,16 +1,16 @@
-const cnv = document.getElementById('c'),  
+/* const cnv = document.getElementById('c'),  
     ctx = cnv.getContext('2d'),
     //fourBar = g2(),
     world = g2().clr()
-                .view({cartesian: true, x: 0, y: 0})
+                .view(mec._interactor.view)
                 //.use({grp: fourBar})
-                .nod({x:100, y:100})
-                .grid({color: "grey", size: 20});
+                .grid({color: "grey", size: 20})
+                .nod({x:100, y:100 , draggable:true})
                 //.lin({x1:0,y1:-mec.ursprung.y,x2:0,y2:cnv.height-mec.ursprung.y,ld:[6,3], ls:"black", lw:1})
                 //.lin({x1:-mec.ursprung.x,y1:0,x2:cnv.width,y2:0,ld:[6,3], ls:"black", lw:1});
-    world.exe(ctx);
-
+ */
 const mec = {
+    spline: [],
     x0: 0,
     y0: 0,
     flags: {
@@ -18,44 +18,67 @@ const mec = {
         isDragging: false,
         dirty: false,
     },
-
+    ctx: document.getElementById('c').getContext('2d'),
+    world: g2(),
     init(){
-        const state = {x:this.x0,y:this.y0,cartesian:this.cartesian};
-        this._interactor = canvasInteractor.create(ctx, state);
+        const state = {x:this.x0,y:this.y0,cartesian: true};
+        this._interactor = canvasInteractor.create(this.ctx, state);
         this._selector = g2.selector(this._interactor.evt);
-        this._selector.selectable({x: this.x,y:this.y})
         this._interactor.on('tick', e => this.ontick(e))
-                        .on('pan', e => this.onpan(e))
+                        //.on('pan', e => this.onpan(e))
                         .on('drag', e => this.ondrag(e))
-                        .on('wheel', e => this.onwheel(e))
-                        .on('click', e => {
-                            this._selector.selection = {x: e.x, y: e.y}
-                            this._selector.selection.drag = true;
-                        });
-        this._interactor.startTimer();
-        document.dispatchEvent(new CustomEvent('init'));
+                        .on('pointerdown', e => this.onpointerdown(e))
+                        //.on('pointermove', e => this.onpointermove(e))
+                        //.on('pointerup', e => this.onpointerup(e))
+                        //.on('wheel', e => this.onwheel(e))
+                        .startTimer();
+        this.world.clr()
+                  .view(this._interactor.view)
+                  .grid({color: "grey", size: 20});
+                  //.nod({x:100, y:100 , draggable:true});
+                  console.log(this._interactor)
     },
+
     ontick(e) {
-        document.dispatchEvent(new CustomEvent('tick'));
 /*         if (this.interactive)
             this._g.exe(this._selector); */
-    world.exe(this._selector);
+        this.world.exe(this._selector).ply({pts: this.spline,ls:'blue',lw:3}).exe(this.ctx);
     },
     onpan(e) { 
-        this._interactor.view.x = this.x0 += e.dx;
-        this._interactor.view.y = this.y0 += e.dy;
+        this._interactor.view.x += e.dx;
+        this._interactor.view.y += e.dy;
+         
+        console.log(this._selector)
     },
     ondrag(e) {    // only modify selected geometry here .. do not redraw .. !
         if (this._selector.selection && this._selector.selection.drag) {
             this._selector.selection.drag({x:e.xusr,y:e.yusr,dx:e.dxusr,dy:e.dyusr,mode:'drag'});
-            document.dispatchEvent(new CustomEvent('drag'));
+        }  
+    },
+    onpointerdown(e){
+        this.flags.dirty = true;     
+        console.log(this.flags.dirty);
+        this._interactor.on('pointermove', e => mec.onpointermove(e))
+                        .on('pointerup', mec.onpointerup());
+        console.log(this._interactor.signals)         
+    },
+    onpointermove(e){
+        if(this.flags.dirty) {
+            this.spline.push({x:e.x,y:e.y});
         }
     },
-    onwheel(e) {
+    onpointerup(){
+        console.log(this.spline)
+        this.flags.dirty = false;
+        console.log(this.flags.dirty);
+        this._interactor.remove('pointermove', mec.onpointermove)
+                        .remove('pointerup', mec.onpointerup);
+    }
+    /* onwheel(e) {
         this._interactor.view.x = e.x + e.dscl*(this._interactor.view.x - e.x);
         this._interactor.view.y = e.y + e.dscl*(this._interactor.view.y - e.y);
         this._interactor.view.scl *= e.dscl;
-    }
+    } */
 
 }
 const render = () => {
